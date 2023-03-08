226a8a964b2d2fdf3dc2c678687daf59a29a2312
origami <1071242996@qq.com>
2023-03-08 20:18:59 +0800

    feature:完成postlist组件基本构造

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,26 @@
 <template>
-
+  <div>
+    <div class="loading">
+      <!--      在数据未返回的时候，显示正在loading的gif-->
+      <img src="../assets/loading.gif" v-if="isLoading">
+    </div>
+    <!--    主题帖子列表-->
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PostList"
+  name: "PostList",
+  data() {
+    return {
+      isLoading: false, //初始默认false
+    }
+  }
 }
 </script>
 
